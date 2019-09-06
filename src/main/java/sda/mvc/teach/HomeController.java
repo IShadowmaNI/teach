72f48c1345a7a255729ffbd7df952ae603221c48
IShadowmaNI <IShadowmaNI@users.noreply.github.com>
2019-09-06 16:36:43 +0300

    Added multiple buttons on homepage for multiple operations - 06/09/2019.

--- src/main/java/sda/mvc/teach/HomeController.java
+++ src/main/java/sda/mvc/teach/HomeController.java
@@ -2,9 +2,8 @@ package sda.mvc.teach;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import sda.mvc.teach.dto.MathResultDTO;
 import sda.mvc.teach.dto.SimpleMathDTO;
 import sda.mvc.teach.dto.Value;
 
@@ -38,13 +37,37 @@ public class HomeController {
         return "home"; //numele paginii html din packetul templates
     }
 
-    @PostMapping("/simpleAdd")
+//    @PostMapping("/simpleAdd") - asta a ramas de la prof
+    @RequestMapping(value = "/action", method = RequestMethod.POST, params = "action=simpleAdd") // am folosit linia asta pentru a face mai multe butoane cu diverse operatii matematice definite mai jos
     public String computeMathThing(@ModelAttribute(value = "mathDTO") SimpleMathDTO simpleMathDTO, Model model) {
         Double sum = simpleMathDTO.value1 + simpleMathDTO.value2;
-        model.addAttribute("result", sum);
 
+        model.addAttribute("result", sum);
         return "result";
     }
 
+    @RequestMapping(value = "/action", method = RequestMethod.POST, params = "action=simpleReduction")
+    public String reduction(@ModelAttribute(value = "mathDTO") SimpleMathDTO simpleMathDTO, Model model) {
+        Double dif = simpleMathDTO.value1 - simpleMathDTO.value2;
 
+        model.addAttribute("result", dif);
+        return "result";
+    }
+
+    @RequestMapping(value = "/action", method = RequestMethod.POST, params = "action=pow2o3")
+    public String powerByTwoOrThree(@ModelAttribute(value = "mathDTO") SimpleMathDTO simpleMathDTO, Model model) {
+
+        Double result = 0D;
+        if (simpleMathDTO.value2 == null) {
+            throw new IllegalArgumentException("Value 2 is null!!!");
+        } else if (simpleMathDTO.value1 == null || simpleMathDTO.value1 < 0) {
+            result = Math.pow(simpleMathDTO.value2, 3);
+            model.addAttribute("result", result);
+            return "result";
+        }
+
+        result = Math.pow(simpleMathDTO.value1, 2);
+        model.addAttribute("result", result);
+        return "result";
+    }
 }
